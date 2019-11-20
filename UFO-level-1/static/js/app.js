1f74b1ff308cbbdcdb2fae01953f60aa6a328ae4
Rachel-Chloe Gregory <rachel.chloe.gregory@gmail.com>
2019-11-19 21:26:49 -0600

    finished part 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,56 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+data.forEach((tableData) => {
+    var row = tbody.append("tr");
+    Object.entries(tableData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+
+  var filteredData = tableData.filter(data => data.datetime === inputValue);
+  console.log(filteredData);
+
+  var fixedData = filteredData.map(data => data.datetime,data.city,data.state,data.country,data.shape,data.durationMinutes,data.comments);
+
+  // remove any children from the list to
+  tbody.html("");
+
+  filteredData.forEach((fixedData) => {
+    var row = tbody.append("tr");
+    Object.entries(fixedData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+});
+
+// Select the button
+var clearbutton = d3.select("#clear-filter-btn");
+
+clearbutton.on("click", function() {
+    tbody.html("");
+    data.forEach((tableData) => {
+        var row = tbody.append("tr");
+        Object.entries(tableData).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+});
